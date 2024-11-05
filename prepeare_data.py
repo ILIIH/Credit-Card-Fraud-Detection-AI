2af0566fa7098feb00532dd277b8a2dfdf7d92cf
illiabranchuk <jointlord0@gmail.com>
2024-11-05 20:56:12 +0000

    refactor data

--- prepeare_data.py
+++ prepeare_data.py
@@ -4,4 +4,6 @@ DATA_PATH = "data/creditcard.csv"
 
 def load_and_refactor_data() : 
     df = pd.read_csv(DATA_PATH)
-    print(df.head())
+    df_output = df[['Class']]
+    df_features = df.drop(columns=['Class'])
+    return df_output,df_features
